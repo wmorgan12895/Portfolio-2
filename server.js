fc62cb02233418fae801a447f17185ef54d9f453
Weston R. Morgan <wmorgan1221@gmail.com>
2016-10-20 15:15:14 -0500

    Made login page

--- server.js
+++ server.js
@@ -1,27 +1,41 @@
 var express = require("express")
 const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
+
+
 var app = express()
-
 app.use(bodyParser.urlencoded({extended: true}))
-app.set('view engine', 'ejs')
-
 
 app.get('/', (req, res) => {
-  db.collection('quotes').find().toArray((err, result) => {
-    if (err) return console.log(err)
-    // renders index.ejs
-    res.render('index.ejs', {quotes: result})
-  })
+  res.sendFile(__dirname + '/login.html')
+})
+
+app.get('/index', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
 })
 
 
-app.post('/quotes', function(req, res){
-  db.collection('quotes').save(req.body, (err, result) => {
-    if (err) return console.log(err)
+app.post('/login', function(req, res){
+  db.collection('users').findOne({email: req.body['email']}, function(err, document){
+      if(document == null) {
+          db.collection('users').save(req.body, (err, result) => {
+          if (err) return console.log(err)
+          console.log('saved to database')
+          res.sendFile(__dirname + '/index.html')
+        })
+      } 
+      else if(err){
 
-    console.log('saved to database')
-    res.redirect('/')
+      }
+      else {
+        if(document.password == req.body['password']){
+          res.send({ code: '200' })
+          console.log("correct login")
+        }else{
+          res.send({ code: '100' })
+          console.log("incorrect login")
+        }
+      }
   })
 })
 
