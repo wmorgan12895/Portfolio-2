a3a3b5f14bffeff1992d5c7e3a159ddac4033595
wrmorgan <wrmorgan@iastate.edu>
2017-01-28 15:24:40 -0600

    work

---
+++ Desktop/Com S 309/Test Heroku Server/test project/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+
+// views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.send("Hello World");
+});
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
